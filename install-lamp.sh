c92b6ad94f7367e0cb1edebf696e0577acd10148
Reinaldo "Naufrago" Moreno <djnaufrago@yandex.com>
2023-07-06 10:01:50 +0200

    Update install-lamp.sh

--- install-lamp.sh
+++ install-lamp.sh
@@ -82,7 +82,7 @@ configfirewall() {
   sudo ufw allow http
   sudo ufw allow https
   sudo echo y | ufw enable
-  # Para Oracle Cloud, comentar los valores anteriores y descomentar los siguiemtes:
+  # For Oracle Cloud, comment out the above values and uncomment the following:
   # sudo iptables -I INPUT 6 -m state --state NEW -p tcp --dport 443 -j ACCEPT
   # sudo iptables -I INPUT 6 -m state --state NEW -p tcp --dport 80 -j ACCEPT
   # sudo netfilter-persistent save
